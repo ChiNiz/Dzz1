ee8e553edef23b217d1f1f2f8f79bbab7ee86cf0
Ching <taibovchingiz@gmail.com>
2021-09-24 20:11:30 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+        int arr[] = {3, 2, 5, 6};
+        int sum = 0;
+
+        /*for (int i = 0; i < arr.length; i++) {  //for
+            sum = sum + arr[i];
+        }
+        System.out.println(sum);*/
+
+        /*int i = 0;   /while
+        while (i < arr.length) {
+            sum = sum + arr[i];
+            i++;
+        }
+        System.out.println(sum);*/
+
+        /*int i = 0;   // do while
+        do {
+            sum = sum + arr[i];
+            i++;
+        } while (i < arr.length);
+        System.out.println(sum);*/
+    }
+
+}
